9d8a3e2479569fd8d5a51f30c36d195ec0e51623
Irum Fazal <irummf@gmail.com>
2018-08-15 16:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log; string.toLowerCase()
 }
-var lowercase = ("hello")
 
-lowercase.toLowerCase()
-=== lowercase
-
-var uppercase = ("HELLO")
-
-uppercase.toUpperCase()
-=== uppercase
 function sayHiToGrandma(string) {
   if (string.toLowerCase)
   return ("I can't hear you.");
